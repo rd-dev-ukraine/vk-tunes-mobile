8a8cd2f58c835c4ef2bd5354d1a04809c7bbf5e6
Sergey Tokarev <sergey.tokarev@hotmail.com>
2016-03-21 17:16:43 +0200

    Install some typings Install bower lib

---
+++ typings/browser.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="browser/ambient/angular-component-router/index.d.ts" />
+/// <reference path="browser/ambient/angular/index.d.ts" />
+/// <reference path="browser/definitions/requirejs/index.d.ts" />
---
+++ typings/main.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="main/ambient/angular-component-router/index.d.ts" />
+/// <reference path="main/ambient/angular/index.d.ts" />
+/// <reference path="main/definitions/requirejs/index.d.ts" />
