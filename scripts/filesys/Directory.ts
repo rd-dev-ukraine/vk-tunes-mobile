710d1dd92d1d58c9a7dbf61b831f973d85d56307
Sergey Tokarev <sergey.tokarev@hotmail.com>
2016-03-25 18:21:19 +0200

    Change directory to es6 promise

--- scripts/filesys/Directory.ts
+++ scripts/filesys/Directory.ts
@@ -1,110 +1,103 @@
 ﻿/// <reference path="../../typings/browser.d.ts"/>
 
 class Directory {
-    static $inject = ["$q", Directory.PathDependency];
     static ServiceName = "directory";
     static PathDependency = "path"
 
-    private directoryContent: FileInfo[]
+    private directoryContent: FileInfo[];
 
-    constructor(
-        private $q: ng.IQService,
-        private path: string) {
+    constructor(private path: string) {
     }
 
-    files(): ng.IPromise<FileInfo[]> {
-        
+    files(): Promise<FileInfo[]> {        
         return this.init().then(() => {
             if (this.directoryContent != null)
                 return this.directoryContent;
             else {
                 return this.readDirectory()
-                    .then(result => {
-                        this.directoryContent = result;
-                        return this.directoryContent;
-                    });
+                           .then(result => {
+                               this.directoryContent = result;
+                                this.directoryContent;
+                           });
             }
         });
     }
 
-    downloadFile(fromUrl: string, fileName: string): ng.IPromise<any> {
+    downloadFile(fromUrl: string, fileName: string, notify: (progressInfo: IFileDownloadingProgress) => void): Promise<any> {
         var deferred = this.$q.defer();
         var folder = this.path;
         var targetPath = folder + "/" + fileName + ".mp3";
 
-        this.init().then(() => {
+        return this.init()
+                   .then(() => new Promise((resolve, reject) => {
+                        var transfer = new FileTransfer();
 
-            var transfer = new FileTransfer();
-
-            transfer.onprogress = (event) => {
-                if (event.lengthComputable) {
-                    var progressInfo: IFileDownloadingProgress = {
-                        percent: Math.round(event.loaded / event.total * 100),
-                        bytesLoaded: event.loaded,
-                        bytesTotal: event.total
-                    };
-                    deferred.notify(progressInfo);
-                }
-            };
-
-            transfer.download(
-                fromUrl,
-                targetPath,
-                file => {
-                    this.directoryContent = null;
-                    deferred.resolve({
-                        path: file.fullPath,
-                        name: file.name
-                    });
-                },
-                error => {
-                    deferred.reject(error);
-                },
-                true);
-        });
-
-        return deferred.promise;
-    }
-
-    private init(): ng.IPromise<any> {
-        var deferred = this.$q.defer();
-
-        window.requestFileSystem(
-            1,
-            0,
-            fs => deferred.resolve(fs),
-            error => deferred.reject(error));
-
-        return deferred.promise;
-    }
-
-    private readDirectory(): ng.IPromise<FileInfo[]> {
-        var deferred = this.$q.defer();
-        
-        window.resolveLocalFileSystemURI(
-            this.path,
-            (dirEntry: DirectoryEntry) => {
-                dirEntry.createReader()
-                    .readEntries(entries => {
-                        var result = [];
-
-                        for (var i in entries) {
-                            var entry = entries[i];
-                            if (entry.isFile) {
-                                result.push({
-                                    path: entry.fullPath,
-                                    name: entry.name
-                                });
+                        transfer.onprogress = (event) => {
+                            if (event.lengthComputable) {
+                                var progressInfo: IFileDownloadingProgress = {
+                                    percent: Math.round(event.loaded / event.total * 100),
+                                    bytesLoaded: event.loaded,
+                                    bytesTotal: event.total
+                                };
+                                
+                                if (notify)
+                                    notify(progressInfo);
                             }
-                        }
+                        };
 
-                        deferred.resolve(result);
-                    },
-                        error => deferred.reject(error));
-            },
-            error => deferred.reject(error));
+                        transfer.download(fromUrl,
+                                            targetPath,
+                                            file => {
+                                                this.directoryContent = null;                                                 
+                                                resolve({
+                                                    path: file.fullPath,
+                                                    name: file.name
+                                                });
+                                            },
+                                            error => {
+                                                reject(error);
+                                            },
+                                            true);
+                   });
+    }
 
-        return deferred.promise;
+    private init(): Promise<any> {        
+        return new Promise((resolve, reject) => {
+            window.requestFileSystem(
+                1,
+                0,
+                fs => resolve(fs),
+                error => reject(error));
+        });
+    }
+
+    private readDirectory(): Promise<FileInfo[]> {
+        
+        return new Promise((resolve, reject) => {
+            
+            window.resolveLocalFileSystemURI(
+                this.path,
+                (dirEntry: DirectoryEntry) => {
+                    dirEntry.createReader()
+                            .readEntries(entries => {
+                                var result = [];
+
+                                for (var i in entries) {
+                                    var entry = entries[i];
+                                    if (entry.isFile) {
+                                        result.push({
+                                            path: entry.fullPath,
+                                            name: entry.name
+                                        });
+                                    }
+                                }
+
+                                resolve(result);
+                            },
+                            error => reject(error));
+                },
+                error => reject(error));
+        });
     }
 }
 
