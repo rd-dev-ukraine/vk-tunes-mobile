7aaf646e5bba004b7505ec43991edce2e4da4d9c
Sergey Tokarev <seregat1984@gmail.com>
2016-03-28 22:51:36 +0300

    Selection on tap for list

--- scripts/components/ListComponent.ts
+++ scripts/components/ListComponent.ts
@@ -4,77 +4,83 @@ import PS = require("../pub-sub/Decorators");
 @PS.Subscriber
 class ListComponentController {
     static ControllerName = "ListComponentController";
-    static $inject = ["$timeout"];
-    
+    static $inject = ["$scope", "$timeout"];
+
     private selectionTogglePromise: ng.IPromise<any>;
-    
+
     items: any[] = [];
     selectedItems: any[] = [];
-    
+
     selectionMode: boolean = false;
-    
-    constructor(private $timeout: ng.ITimeoutService) {        
+
+    constructor(private $scope: ng.IScope,
+        private $timeout: ng.ITimeoutService) {
     }
-    
+
     isSelected(item: any): boolean {
         return (this.selectedItems || []).some(e => e === item);
     }
-    
+
     toggleSelection(item: any) {
         if (!this.selectionMode)
             return;
-        
-        if(this.isSelected(item))
-            this.selectedItems = this.selectedItems.filter(e => e !== item) || [];            
+
+        this.selectedItems = this.selectedItems || [];
+
+        if (this.isSelected(item))
+            this.selectedItems = this.selectedItems.filter(e => e !== item) || [];
         else
-            this.selectedItems.push(item);   
-            
-        this.onSelection({ selectedItems: this.selectedItems });
+            this.selectedItems.push(item);
     }
-    
+
     beginToggleSelection() {
         if (this.selectionMode)
-            return;           
-        
+            return;
+
         this.selectionTogglePromise = this.$timeout(1000, true);
-        
+
         this.selectionTogglePromise
             .then(() => {
-                this.selectionMode = true;                
+                this.selectionMode = true;
             });
     }
-    
+
     cancelToggleSelection() {
         if (this.selectionTogglePromise)
             this.$timeout.cancel(this.selectionTogglePromise);
     }
-    
-    onSelection(param: any) {}
 }
 
 export var Component: ng.IComponentOptions = {
     bindings: {
         items: "<",
         selectionMode: "=",
-        onSelection: "&"
-    }, 
+        selectedItems: "="
+    },
     controller: ListComponentController,
     controllerAs: "$c",
     template: `
     <ul >
         <li class="list-item"
             ng-repeat="$item in $c.items">
-            <div class="list-item__container">
-                <div class="list-item__selector"
-                     style="float: left"
-                     ng-show="$c.selectionMode">
-                    <input type="checkbox"
-                           ng-checked="$c.isSelected($item)"
-                           ng-click="$c.toggleSelection($item)" />
+            <div ng-mousedown="$c.beginToggleSelection()" 
+                 ng-mouseup="$c.cancelToggleSelection()" 
+                 ng-mousemove="$c.cancelToggleSelection()"
+                 ng-touchstart="$c.beginToggleSelection()" 
+                 ng-touchend="$c.cancelToggleSelection()" 
+                 ng-touchmove="$c.cancelToggleSelection()">
+                <div class="list-item__container"
+                    ng-click="$c.toggleSelection($item)">
+                    <div class="list-item__selector"
+                        style="float: left"
+                        ng-show="$c.selectionMode">
+                        <input type="checkbox"
+                            ng-checked="$c.isSelected($item)" />
+                    </div>
+                    <div ng-transclude></div>
                 </div>
-                <div ng-transclude></div>
             </div>
-            <hr />            
+            <hr />
         <li>
     </ul>`,
     transclude: true
